ff310e904f7cd60d7375832e738007ac93358682
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-16 14:05:17 +0200

    Added BankAccount Controller with views. Added BankAccountViewModel. Reworked views to use viewmodel

---
+++ HomeBudget2/Controllers/BankAccountsController.cs
@@ -0,0 +1,131 @@
+﻿using HomeBudget2.DAL.Interfaces;
+using HomeBudget2.ViewModels;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace HomeBudget2.Controllers
+{
+    public class BankAccountsController : Controller
+    {
+        private readonly IBankAccountRepository _bankAccountRepository;
+
+        public BankAccountsController(IBankAccountRepository bankAccountRepository)
+        {
+            _bankAccountRepository = bankAccountRepository;
+        }
+
+        // GET: BankAccounts
+        public ActionResult Index()
+        {
+            var bankaccountVm = new BankAccountViewModel();
+            bankaccountVm.BankAccountsList = _bankAccountRepository.GetWhere(bankAccount => bankAccount.Id > 0);
+
+            return View(bankaccountVm);
+        }
+
+        // GET: BankAccounts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var bankaccountVm = new BankAccountViewModel();
+            bankaccountVm.BankAccount = _bankAccountRepository.GetWhere(bankAccount => bankAccount.Id == id).FirstOrDefault();
+            if (bankaccountVm.BankAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bankaccountVm);
+        }
+
+        // GET: BankAccounts/Create
+        public ActionResult Create()
+        {
+            var bankaccountVm = new BankAccountViewModel();
+            bankaccountVm.BankAccountsList = _bankAccountRepository.GetWhere(bankAccount => bankAccount.Id > 0);
+
+            return View(bankaccountVm);
+        }
+
+        // POST: BankAccounts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BankAccountViewModel bankAccountVm)
+        {
+            if (ModelState.IsValid)
+            {
+                _bankAccountRepository.Create(bankAccountVm.BankAccount);
+                return RedirectToAction("Index");
+            }
+
+            return View(bankAccountVm);
+        }
+
+        // GET: BankAccounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var bankaccountVm = new BankAccountViewModel();
+            bankaccountVm.BankAccount = _bankAccountRepository.GetWhere(bankAccount => bankAccount.Id == id).FirstOrDefault();
+            if (bankaccountVm.BankAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bankaccountVm);
+        }
+
+        // POST: BankAccounts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BankAccountViewModel bankAccountVm)
+        {
+            if (ModelState.IsValid)
+            {
+                _bankAccountRepository.Update(bankAccountVm.BankAccount);
+                return RedirectToAction("Index");
+            }
+            return View(bankAccountVm);
+        }
+
+        // GET: BankAccounts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var bankaccountVm = new BankAccountViewModel();
+
+            bankaccountVm.BankAccount = _bankAccountRepository.GetWhere(bankAccount => bankAccount.Id == id).FirstOrDefault();
+            if (bankaccountVm.BankAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bankaccountVm);
+        }
+
+        // POST: BankAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var bankaccountVm = new BankAccountViewModel();
+
+            bankaccountVm.BankAccount = _bankAccountRepository.GetWhere(bankAccount => bankAccount.Id == id).FirstOrDefault();
+
+            _bankAccountRepository.Delete(bankaccountVm.BankAccount);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ HomeBudget2/ViewModels/BankAccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HomeBudget2.Models;
+
+namespace HomeBudget2.ViewModels
+{
+    public class BankAccountViewModel
+    {
+        public BankAccount BankAccount { get; set; }
+        public List<BankAccount> BankAccountsList { get; set; }
+    }
+}
