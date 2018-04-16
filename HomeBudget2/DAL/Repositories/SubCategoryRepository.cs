492864414893a9793d1ad0f4a800c2e20eab2275
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-16 17:20:24 +0200

    Added SubCategory Repository, Irepository and controller with views

---
+++ HomeBudget2/DAL/Repositories/SubCategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using HomeBudget2.DAL.Interfaces;
+using HomeBudget2.Models;
+
+namespace HomeBudget2.DAL.Repositories
+{
+    public class SubCategoryRepository : AbstractRepository<SubCategory>, ISubCategoryRepository
+    {
+    }
+}
