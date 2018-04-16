0e2394c7c3451439fd7639130deb48d4380b042c
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-16 11:58:46 +0200

    Created BankAccountRepository and interface for it

--- HomeBudget2/DAL/Repositories/AbstractRepository.cs
+++ HomeBudget2/DAL/Repositories/AbstractRepository.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using HomeBudget2.DAL.Interfaces;
+using HomeBudget2.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Web;
-using HomeBudget2.DAL.Interfaces;
-using HomeBudget2.Models;
 
 namespace HomeBudget2.DAL.Repositories
 {
