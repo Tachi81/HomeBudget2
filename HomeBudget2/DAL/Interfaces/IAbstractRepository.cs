b14865c3761bf4066162cbc81f94a30a7dbe07d8
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-16 11:40:23 +0200

    Added Abstract Repository and interface for it

---
+++ HomeBudget2/DAL/Interfaces/IAbstractRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace HomeBudget2.DAL.Interfaces
+{
+    public interface IAbstractRepository<T> where T : class
+    {
+        void Create(T entity);
+        void Delete(T entity);
+        List<T> GetWhere(Expression<Func<T, bool>> expression);
+        void Update(T entity);
+        List<T> GetWhereWithIncludes(Expression<Func<T, bool>> expressionWhere,
+            params Expression<Func<T, object>>[] includes);
+    }
+}
---
+++ HomeBudget2/DAL/Repositories/AbstractRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using HomeBudget2.DAL.Interfaces;
+using HomeBudget2.Models;
+
+namespace HomeBudget2.DAL.Repositories
+{
+    public class AbstractRepository<T> : IAbstractRepository<T> where T : class
+    {
+        public virtual void Create(T entity)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Set<T>().Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public virtual void Delete(T entity)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+
+                context.Entry(entity).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public virtual List<T> GetWhere(Expression<Func<T, bool>> expression)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var query = context.Set<T>().Where(expression);
+                return query.ToList();
+            }
+        }
+
+        public virtual List<T> GetWhereWithIncludes(Expression<Func<T, bool>> expressionWhere, params Expression<Func<T, object>>[] includes)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                IQueryable<T> query = context.Set<T>();
+                foreach (Expression<Func<T, object>> include in includes)
+                    query = query.Include(include);
+                query = query.Where(expressionWhere);
+                return query.ToList();
+            }
+        }
+
+        public virtual void Update(T entity)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+
+                context.Entry(entity).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
