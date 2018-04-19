e631a2ef0be17fb87a3ee80aa3efa581426f6ac6
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-19 09:33:39 +0200

    Added datepicker

--- HomeBudget2/App_Start/BundleConfig.cs
+++ HomeBudget2/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace HomeBudget2
 {
@@ -14,6 +13,9 @@ namespace HomeBudget2
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jQuery-UI").Include(
+                "~/Scripts/jquery-ui-{version}.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +28,12 @@ namespace HomeBudget2
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datepicker").Include(
+                "~/Scripts/DatePicker.js"));
+
+            bundles.Add(new StyleBundle("~/jQuery-UI/css").Include(
+                    "~/Content/themes/base/jquery-ui.min.css"));
         }
     }
 }
---
+++ HomeBudget2/ViewModels/FinancialOperationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeBudget2.Models;
+
+namespace HomeBudget2.ViewModels
+{
+    public class FinancialOperationViewModel
+    {
+        public FinancialOperation FinancialOperation { get; set; }
+        public List<FinancialOperation> ListOfFinancialOperations { get; set; }
+
+        public IEnumerable<SelectListItem> SelectListOfBankAccounts{ get; set; }
+        public IEnumerable<SelectListItem> SelectListOfCategories { get; set; }
+        public IEnumerable<SelectListItem> SelectListOfSubCategories { get; set; }
+    }
+}
